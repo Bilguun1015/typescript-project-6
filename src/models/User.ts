acad5b6b6b0e85733081dbe1d2759917d97d4831
Bilguun <bbg.1015@gmail.com>
2020-07-27 12:37:33 -0500

    added fetch method on User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,8 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
   // ? makes properties optional
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -35,5 +38,13 @@ export class User {
     handlers.forEach((callback) => {
       callback();
     });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
   }
 }
