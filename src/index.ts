e4918f05a4bdb06cdd4c17e19efa2ff0d5273525
Bilguun <bbg.1015@gmail.com>
2020-07-29 15:12:16 -0500

    created abstract class View to make some reusable code

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { User } from './models/User';
 const user = User.buildUser({ name: 'NAME', age: 20 });
 
 const root = document.getElementById('root');
-
+// setting type guard for root
 if (root) {
   const userForm = new UserForm(root, user);
 
