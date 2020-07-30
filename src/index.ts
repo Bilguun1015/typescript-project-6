1105d16695741c97cd2eb277729af36c28122f62
Bilguun <bbg.1015@gmail.com>
2020-07-30 13:25:36 -0500

    created userlist to display the users from API

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById('root');
-// setting type guard for root
-if (root) {
-  const userEdit = new UserEdit(root, user);
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found');
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
